72e7dc4d821b09139ba90a999c6ea7da672f80a4
Markus Moilanen <moilamar@protonmail.com>
2017-11-08 23:28:36 +0200

    footer image

--- wp-content/themes/yummy-child/footer.php
+++ wp-content/themes/yummy-child/footer.php
@@ -35,7 +35,28 @@ do_action( 'yummy_footer_start' );
  * @hooked yummy_footer_bottom_end - 25
  *
  */
-do_action( 'yummy_footer' );
+
+
+
+
+ /* Custom footer */
+ ?>
+ <footer id="colophon" class="site-footer col-1" role="contentinfo">
+ <div class="bottom-footer page-section" style="background-image: url('http://localhost/hunajawp/wordpress/wp-content/themes/yummy-child/assets/uploads/site-info.jpg');">
+ <div class="wrapper">
+ <div class="social-link">
+ <ul id="menu-footer" class="social-icons">
+ <li id="menu-item-48" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-48">
+ <a href="http://localhost/hunajawp/wordpress/ota-yhteytta/">Ota Yhteyttä</a></li></ul></div><div class="site-info"> 
+ <span class="copyright">Copyright © 2017. All Rights Reserved</span> <span><a href="">Mesiäinen
+ </a></span></div></div></div></footer>
+<?php
+
+
+
+
+
+
 
 /**
  * yummy_footer_end hook
