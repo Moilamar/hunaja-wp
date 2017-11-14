f7f624f3cf6099c6d5dbde6a25682cc99579ce9b
Markus Moilanen <moilamar@protonmail.com>
2017-11-15 01:07:28 +0200

    -

--- wp-content/themes/yummy-child/page.php
+++ wp-content/themes/yummy-child/page.php
@@ -47,6 +47,40 @@
     get_footer();    
  }
 
+ else if ($pagename == "harjoitustyodokumentaatio") {
+    global $post;
+    get_header(); 
+    if ( true === apply_filters( 'yummy_filter_frontpage_content_enable', true ) ) : 
+    ?>
+        <div class="wrapper page-section">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main front-page-posts" role="main">
+    
+                    <?php
+                    query_posts('category=Dokumentaatio');
+                    while ( have_posts() ) : the_post();
+    
+                        get_template_part( 'template-parts/content', 'page' );
+    
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) :
+                            comments_template();
+                        endif;
+    
+                    endwhile; // End of the loop.
+                    ?>
+    
+                </main><!-- #main -->
+            </div><!-- #primary -->
+            <?php
+            if ( yummy_is_sidebar_enable() ) {
+                get_sidebar();
+            } ?>
+        </div><!-- .wrapper/.page-section-->
+    <?php endif;
+    
+    get_footer();
+ }
 
 else {
     global $post;
